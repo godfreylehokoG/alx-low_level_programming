eb9241b7d10b450dc9c4012ce88febd135181f3d
Lehlohonolo Lehoko <hlonilehoko29@yahoo.com>
2022-03-10 16:09:35 -0800

    6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+	long int longintType;
+	long long int longlongintType;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	return (0);
+}
