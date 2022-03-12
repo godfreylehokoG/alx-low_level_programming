7f0669bc387c19b8a980240b34c4be0cd0b0abe5
Lehlohonolo Lehoko <hlonilehoko29@yahoo.com>
2022-03-11 19:23:19 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	printf("\n");
+
+	return (0);
+}
