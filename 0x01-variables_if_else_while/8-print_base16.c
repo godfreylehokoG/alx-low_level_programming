484c110376d20c8236b19e48fec2989f5d487c3b
Lehlohonolo Lehoko <hlonilehoko29@yahoo.com>
2022-03-14 15:30:05 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	i = 48;
+	j = 'a';
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j <= 'f')
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
