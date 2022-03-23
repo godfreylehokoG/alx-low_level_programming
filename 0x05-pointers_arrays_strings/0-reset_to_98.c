148ac8163ea0d83ceec053842c263e39d6cd0cba
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-03-23 09:52:18 +0200

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * reset_to_98 - returns the largest of 3 numbers
- * @*n: char to check if is upper case
- * Return: 1 if is upper case 0 if not
+ * reset_to_98 - resets the given number to 98
+ * @n: char to check if is upper case
+ * Return: void function
  */
 void reset_to_98(int *n)
 {
 	int i;
 
 	i = 98;
-	*n = (* &i);
+	*n = (*&i);
 }
