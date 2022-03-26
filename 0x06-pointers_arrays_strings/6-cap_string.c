3a9138f46448a5d9e8b12fc2891b640273b24fc7
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-03-26 03:27:35 +0200

    6. Expect the best. Prepare for the worst. Capitalize on what comes [1]

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - copies a string
+ * @a: str to convert toupper
+ * Return: a
+ */
+
+char *cap_string(char *a)
+{
+	char b[] = "\t\n,;.!?\"(){}";
+	int i; 
+	int j;
+
+	i = 0;
+	j = 0;
+	while (a[i] != '\0')
+	{
+		if (a[i] > 96 && a[i] < 123)
+		{
+			if (i == 0)
+				a[i] -= 32;
+
+			else
+			{
+				while (b[j] != '\0')
+				{
+					if (a[i - 1] == b[j])
+						a[i] -= 32;
+					j++;
+				}
+			}
+		}
+		i++;
+	}
+	return (a);
+}
