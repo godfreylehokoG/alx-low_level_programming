54f50518caa7651fdbcfdf032770d4c2154396e2
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-03-30 14:04:07 +0200

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget [2]

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,18 +8,5 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i++]);
-	}
-/*	_putchar(s[i]);
-	i++;
-	if (s[i] != '\0')
-	{
-		_putchar(s[i] * _puts_recursion(s[i + 1]));
-	}
-*/	_putchar(10);
+	puts(s);
 }
