fa6d234a028f40287ce1e5ca586d6da25ec96d56
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-03-31 18:14:59 +0200

    1. Why is it so important to dream? Because, in my dreams we are together [1]

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - str to print
+ * @s: str to factorize
+ * Return: void function
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != 0)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
