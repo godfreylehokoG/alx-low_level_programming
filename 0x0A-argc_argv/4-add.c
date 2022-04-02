09f68bd924fad0090f3910f4e531e9112ca6d570
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-04-02 02:04:45 +0200

    4. To infinity and beyond [1]

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
 /**
+ * isnumber - checks str if is number
+ * @s: str to check if isnumber
+ * Return: 1 if is number 0 if not
+ */
+
+int isnumber(char *s)
+{
+	if (*s > 47 && *s < 58)
+		return (1);
+	return (0);
+}
+
+/**
  * main - check the code
  * @argc: size of argv array
  * @argv: array of arguments
@@ -9,17 +22,20 @@
 
 int main(int argc, char **argv)
 {
-	int i, sum;
+	int sum, i = 0;
 
-	if (argc > 0)
+	if (argc >= 0)
 	{
-		for (i = 1; argv[i] > 0; i++)
+		for (i = 1; i < argc; i++)
 		{
+			if (isnumber(argv[i]) != 1)
+			{
+				printf("Error\n");
+				return (-1);
+			}
 			sum += atoi(argv[i]);
-			printf("%d\n", sum);
 		}
-		return (0);
+		printf("%d\n", sum);
 	}
-	printf("Error\n");
-	return (1);
+	return (0);
 }
