a6e387feadaf787dc9e74420a35f749c8da716ec
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-04-08 23:27:26 +0200

    0. Trust no one [3]

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,8 +11,7 @@ void *malloc_checked(unsigned int b)
 	int *malloc_int;
 
 	malloc_int = malloc(b);
-	if (malloc_int == 0)
+	if (malloc_int == NULL)
 		exit(98);
-	malloc_int[0] = b;
 	return (malloc_int);
 }
