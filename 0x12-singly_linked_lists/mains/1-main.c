5fe2b020aed58cbd03758e009414f82c224c6ac5
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-04-21 22:05:59 +0200

    0. Print list [2]

--- 0x12-singly_linked_lists/mains/1-main.c
+++ 0x12-singly_linked_lists/mains/1-main.c
@@ -1,4 +1,4 @@
-#include "../lists.h"
+#include "../list.h"
 
 /**
  * main - check the code
@@ -23,15 +23,9 @@ int main(void)
     new->len = 5;
     new->next = head;
     head = new;
-    n = print_list(head);
+    n = list_len(head);
     printf("-> %lu elements\n", n);
-
-    printf("\n");
     free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
     free(new);
     return (0);
 }
