75bc15bcd80080a30899851a55a3aaed8e694ce7
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-04-21 21:53:25 +0200

    0. Print list [1]

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "list.h"
+
+/**
+ * print_list - prints all elements of a list
+ * @h: node to be printed
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *nodes = h;
+	int i = 0;
+
+	while (nodes)
+	{
+		printf("[%u] %s\n", nodes->len, nodes->str);
+		nodes = nodes->next;
+		i++;
+	}
+	return (0);
+}
