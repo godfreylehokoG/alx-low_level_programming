c3ced8e7eaf9fd178d27acbf9b2dff34f80f7cca
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-04-22 01:11:22 +0200

    2. Add node [1]

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * list_len - list elements of in linked list
+ * @head: node to be printed
+ * @str: 
+ *
+ * Return: number of nodes
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{	
+	list_t *new_node;
+	unsigned int i = 0;
+
+	while (str[i])
+		i++;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = i;
+	new_node->next = *head;
+	*head = new_node;
+	`
+	return (new_node);
+}
