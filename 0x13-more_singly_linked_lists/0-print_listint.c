74bbcf2e042e56bd5e8f2018fe899924d22c55d7
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-04-25 21:26:02 +0200

    1. List length [1]

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints a list
+ * @h: items to be listed
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
