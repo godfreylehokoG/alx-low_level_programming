932857c34a1559148c5a89f20f45dfb88a84f5eb
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-04-25 10:34:27 +0200

    0. Poppy [1]

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
