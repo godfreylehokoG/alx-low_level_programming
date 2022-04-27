000134918ec197092eb9325a7515dbe0730bb79a
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-04-27 22:15:05 +0200

    11. Reverse list [1]

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,14 @@
+#include "list.h"
+
+/**
+ * reverse_listint - reverses s function
+ * @head: list to be reversed
+ *
+ * Return: reversed int.
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
