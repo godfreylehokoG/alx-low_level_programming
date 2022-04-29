e361de0563f4eb3b019a59a3450a04810a3bb18f
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-04-30 01:38:24 +0200

    0. 0 [1]

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * print_listint - reverses s function
+ * @head: list to be reversed
+ *
+ * Return: reversed int.
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	head++;
+	return (0);
+}
