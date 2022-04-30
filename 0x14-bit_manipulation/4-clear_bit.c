4c877324fcc2669d89dea1d30058a7d1a30e7acf
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-04-30 03:10:52 +0200

    4.100 [2]

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * clear_bit - sets the value to 0
+ * @n: int to clear
+ * @index: starts from 0
+ *
+ * Return: 1 if worked -1 if not.
+ */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int count = 0;
