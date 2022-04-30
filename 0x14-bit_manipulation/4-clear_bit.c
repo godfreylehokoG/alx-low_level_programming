3c95af4268d3a0124fa7f7f1c4c57987eca95343
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-04-30 03:03:33 +0200

    4.100 [1]

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int count = 0;
+	unsigned long copy = *n;
+
+	while (copy)
+	{
+		count++;
+		copy >>= 1;
+	}
+
+	if (index > 24)
+		return (-1);
+
+	*n &= ~(1UL << index);
+
+	return (1);
+}
