60f77dcbd6f8102376e45f744091a663062194c6
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-04-30 02:06:24 +0200

    1. 1 [1]

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _putchar - prints char
+ * @c: char to print
+ *
+ * Return: void function.
+ */
+
+int _putchar(char c)
+{
+	write (1, &c, 1);
+	return (0);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -12,5 +12,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 int get_endianness(void);
+int _putchar(char c);
 
 #endif
