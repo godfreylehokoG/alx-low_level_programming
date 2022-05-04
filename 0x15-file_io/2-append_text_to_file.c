74b2c454e3232d1bc266d2865ea57a6946d3bfbf
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-05-04 03:56:44 +0200

    2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - reads text file and prints to POSIX
+ * @filename: file to read from
+ * @text_content: letters to be read
+ * Return: 1 if successful -1 if not..
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, wrt, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		fd = open(filename, O_WRONLY | O_APPEND);
+		if (fd == -1)
+			return (-1);
+
+		if (text_content == NULL)	
+		{
+			close(fd);
+			return (1);
+		}
+
+
+		while (text_content[len])
+			len++;
+		wrt = write(fd, text_content, len);
+		if (wrt == -1 || wrt != len)
+		{
+			close(fd);
+			write(STDOUT_FILENO, "fails", 5);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
