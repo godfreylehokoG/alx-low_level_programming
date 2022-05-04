b504dbbaf844fc821fd2bdb80134b27dcd9418be
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-05-04 03:30:00 +0200

    6

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 		wrt = write(fd, text_content, len);
 		if (wrt == -1)
 		{
-			write(1, "fails", 5);
+			write(1, "fails", 6);
 			return (-1);
 		}
 	}
