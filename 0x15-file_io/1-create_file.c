00417b8bcbe4b883f9144f0d11b7273a4bf9351e
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-05-04 03:04:17 +0200

    1. Under the snow [3]

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL || text_content == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 777);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
@@ -29,8 +29,6 @@ int create_file(const char *filename, char *text_content)
 		write(1, "fails\n", 6);
 		return (-1);
 	}
-
 	close(fd);
-
 	return (1);
 }
