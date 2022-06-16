551eb97d753c1a4e5359b4b59d3364bd99cff503
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-06-16 15:08:06 +0200

    1. Without libraries what have we? We have no past and no future [1]

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -fPIC *.c -shared -o liball.so
+gcc -fPIC -g -c -Wall -pedantic -Werror -Wextra *.c
+gcc -shared -Wl,-soname,liball.so -o liball.so *.o -lc
