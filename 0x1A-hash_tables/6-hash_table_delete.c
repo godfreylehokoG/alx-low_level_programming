a5a320a8e5eb0ec1fe0f481f5bffa4f0ef38a0e5
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-07-02 03:48:38 +0200

    GisG

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,77 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete: deletes all the contents of and the hash table @ht
- * @ht: hash table to be deleted
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to delete
  *
  * Return: void
  */
+
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long i;
+	unsigned long int i;
+	hash_node_t *next;
 
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
 	for (i = 0; i < ht->size; i++)
 	{
-		if (!ht->array[i])
-			continue;
-		free_node(ht->array[i]);
+		while (ht->array[i] != NULL)
+		{
+			next = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = next;
+		}
 	}
-	free(*ht->array);
 	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
 	free(ht);
-}
-
-/**
- * free_node - deletes the node @head from the heap
- * @head: the node to be deleted
- *
- * Return: void
- */
-
-void free_node(hash_node_t *head)
-{
-	hash_node_t **all_nodes = NULL;
-	hash_node_t *first_node = head;
-	int i, nodes = 0;
-
-	for (; head != NULL; nodes++)
-	{
-		head = head->next;
-	}
-	nodes--;
-	head = first_node;
-
-	if (nodes == 0)
-	{
-		free(head->value);
-		free(head->key);
-		free(head->next);
-		free(head);
-		return;
-	}
-	all_nodes = malloc(sizeof(hash_node_t) * nodes);
-	for (i = 0; i < nodes; i++)
-	{
-		all_nodes[i] = head;
-		head = head->next;
-	}
-
-	for (i = nodes - 1; i >= 0; i -= 2)
-	{
-		free(all_nodes[i]->key);
-		free(all_nodes[i]->value);
-		free(all_nodes[i]->next->key);
-		free(all_nodes[i]->next->value);
-		free(all_nodes[i]->next);
-		free(all_nodes[i]);
-	}
-
-	if ((nodes - 1) % 2)
-	{
-		free(all_nodes[0]->key);
-		free(all_nodes[0]->value);
-		free(all_nodes[0]->next);
-		free(all_nodes[0]);
-	}
-	free(all_nodes);
 }
